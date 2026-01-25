1a232fe696dfbd4eac97e4f96b5386d2b00907d7
Majid Golshadi <golshadi.majid@gmail.com>
2026-01-25 10:53:52 +0100

    binary tree inorder traversal

--- easy/94-binary_tree_inorder_traversal/main.go
+++ easy/94-binary_tree_inorder_traversal/main.go
@@ -8,71 +8,102 @@ import (
 func main() {
 
 	tests := []struct {
-		input1         []int
-		input1_length  int
-		input2         []int
-		input2_length  int
+		input          *TreeNode
 		expectedOutput []int
 	}{
 		{
-			input1:         []int{1, 2, 3, 0, 0, 0},
-			input1_length:  3,
-			input2:         []int{2, 5, 6},
-			input2_length:  3,
-			expectedOutput: []int{1, 2, 2, 3, 5, 6},
+			input:          nil,
+			expectedOutput: nil,
 		},
 		{
-			input1:         []int{1},
-			input1_length:  1,
-			input2:         []int{},
-			input2_length:  0,
+			input: &TreeNode{
+				Val: 1,
+			},
 			expectedOutput: []int{1},
 		},
 		{
-			input1:         []int{0},
-			input1_length:  0,
-			input2:         []int{1},
-			input2_length:  1,
-			expectedOutput: []int{1},
+			input: &TreeNode{
+				Val:  1,
+				Left: nil,
+				Right: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val:   3,
+						Left:  nil,
+						Right: nil,
+					},
+				},
+			},
+			expectedOutput: []int{1, 3, 2},
 		},
 		{
-			input1:         []int{4, 0, 0, 0, 0, 0},
-			input1_length:  1,
-			input2:         []int{1, 2, 3, 5, 6},
-			input2_length:  5,
-			expectedOutput: []int{1, 2, 3, 4, 5, 6},
-		},
-		{
-			input1:         []int{4, 5, 6, 0, 0, 0},
-			input1_length:  3,
-			input2:         []int{1, 2, 3},
-			input2_length:  3,
-			expectedOutput: []int{1, 2, 3, 4, 5, 6},
+			input: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val: 4,
+					},
+					Right: &TreeNode{
+						Val: 5,
+						Right: &TreeNode{
+							Val: 7,
+						},
+						Left: &TreeNode{
+							Val: 6,
+						},
+					},
+				},
+				Right: &TreeNode{
+					Val: 3,
+					Right: &TreeNode{
+						Val: 8,
+						Left: &TreeNode{
+							Val: 9,
+						},
+					},
+				},
+			},
+			expectedOutput: []int{4, 2, 6, 5, 7, 1, 3, 9, 8},
 		},
 	}
 
 	for _, test := range tests {
-		merge(test.input1, test.input1_length, test.input2, test.input2_length)
+		output := inorderTraversal(test.input)
 
-		if reflect.DeepEqual(test.expectedOutput, test.input1) {
-			log.Fatalf("output is %v but expected is %v", test.input1, test.expectedOutput)
+		if reflect.DeepEqual(test.expectedOutput, output) {
+			log.Fatalf("output is %v but expected is %v for input %v", output, test.expectedOutput, test.input)
 		}
 	}
 
 }
 
-func merge(nums1 []int, m int, nums2 []int, n int) {
-	for i, j := 0, m; i < n; i, j = i+1, j+1 {
-		nums1[j] = nums2[i]
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// Constraints:
+//  * The number of nodes in the tree is in the range [0, 100].
+//  * -100 <= Node.val <= 100
+
+// left -> root -> right
+func inorderTraversal(root *TreeNode) []int {
+	var result []int
+	var traverse func(root *TreeNode)
+
+	traverse = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		traverse(root.Left)
+		result = append(result, root.Val)
+		traverse(root.Right)
 	}
 
-	for j := 0; j < m+n-1; j++ {
-		for i := 0; i < m+n-1; i++ {
-			if nums1[i] > nums1[i+1] {
-				b := nums1[i]
-				nums1[i] = nums1[i+1]
-				nums1[i+1] = b
-			}
-		}
-	}
+	traverse(root)
+
+	return result
 }
